411bdf2d40b14378ae5acdf4c80437b3ade937e4
saucyy <131738356+Berrytree5@users.noreply.github.com>
2023-10-25 16:24:49 -0700

    overhaul and rewrites

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,28 +1,84 @@
 const router = require('express').Router();
 const { Category, Product } = require('../../models');
 
-// The `/api/categories` endpoint
-
-router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+// Get all categories with associated Products
+router.get('/', async (req, res) => {
+  try {
+    const categories = await Category.findAll({
+      include: Product,
+    });
+    res.status(200).json(categories);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: 'Server error' });
+  }
 });
 
-router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+
+router.get('/:id', async (req, res) => {
+  try {
+    const category = await Category.findByPk(req.params.id, {
+      include: Product,
+    });
+
+    if (!category) {
+      return res.status(404).json({ message: 'Category not found' });
+    }
+
+    res.status(200).json(category);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: 'Server error' });
+  }
 });
 
-router.post('/', (req, res) => {
-  // create a new category
+// Create a new category
+router.post('/', async (req, res) => {
+  try {
+    const newCategory = await Category.create(req.body);
+    res.status(201).json(newCategory);
+  } catch (err) {
+    console.error(err);
+    res.status(400).json({ message: 'Invalid request' });
+  }
 });
 
-router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+
+router.put('/:id', async (req, res) => {
+  try {
+    const [updatedRows] = await Category.update(
+      { category_name: req.body.category_name },
+      { where: { id: req.params.id } }
+    );
+
+    if (updatedRows === 0) {
+      return res.status(404).json({ message: 'Category not found' });
+    }
+
+    res.status(200).json({ message: 'Category updated successfully' });
+  } catch (err) {
+    console.error(err);
+    res.status(400).json({ message: 'Invalid request' });
+  }
 });
 
-router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
+// Delete a category by its `id` value
+router.delete('/:id', async (req, res) => {
+  try {
+    const deletedCategory = await Category.findByPk(req.params.id);
+
+    if (!deletedCategory) {
+      return res.status(404).json({ message: 'Category not found' });
+    }
+
+    await Product.destroy({ where: { category_id: req.params.id } });
+    await deletedCategory.destroy();
+
+    res.status(200).json({ message: 'Category deleted successfully' });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: 'Server error' });
+  }
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const routes = require('./routes');
-const sequelize = require('./db');
+const sequelize = require('./db'); 
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -11,7 +11,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
-sequelize.sync({ force: false }) 
+sequelize
+  .sync({ force: false })
   .then(() => {
     app.listen(PORT, () => {
       console.log(`App listening on port ${PORT}!`);
