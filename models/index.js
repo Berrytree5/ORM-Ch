df2432da804cff85151876eb9a67026926da1996
saucyy <131738356+Berrytree5@users.noreply.github.com>
2023-10-26 16:21:32 -0700

    minor fix

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 // imports models
 const Product = require('./Product');
-const Category = require('./Category');
+const Category = require('./Category2');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
